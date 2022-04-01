e1e30120d81720067d3b9c41a1ac6bf70afb487b
Brijesh <brijesh.20223@gmail.com>
2022-04-02 00:45:19 +0530

    slide show automate

--- ProjectPBS.js
+++ ProjectPBS.js
@@ -1,5 +1,5 @@
 let slideIndex = 1;
-    showSlides(slideIndex);
+    // showSlides(slideIndex);
     
     function plusSlides(n) {
       showSlides(slideIndex += n);
@@ -22,3 +22,19 @@ let slideIndex = 1;
       dots[slideIndex-1].className += " active";
     }
     // *********************************
+    let n =0;
+    function showSlidesato() {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      if (n > slides.length) {slideIndex = 1}    
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+      setTimeout(showSlidesato, 2000);  
+    }
